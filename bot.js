a21fedbea307afe76e1966d4883c014e0763f9b6
vivinkurien <43483959+vivinkurien@users.noreply.github.com>
2018-09-21 16:17:32 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 //const client = new Discord.Client();
-var BOT_TOKEN = (process.env.BOT_TOKEN); 
+var BOT_TOKEN = process.env.BOT_TOKEN; 
 
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
