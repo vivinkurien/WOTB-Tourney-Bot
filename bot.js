93dcdaf95803b130e11c80e27e491c0bc7e21340
vivinkurien <43483959+vivinkurien@users.noreply.github.com>
2018-09-21 16:29:59 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var BOT_TOKEN = process.env.BOT_TOKEN; 
+var prefix='!';
 
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
@@ -14,11 +15,13 @@ client.on("ready", () => {
 client.on("message", async message => {
     if(message.author.bot) return;
   
-    if(message.content.indexOf(config.prefix) !== 0) return;
+    if(message.content.indexOf(prefix) !== 0) return;
   
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
+  console.log('blaj');
+  console.log(command);
   // Let's go with a few common example commands! Feel free to delete or change those.
   
   if(command === "ping") {
