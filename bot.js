0091e9d3fc0695e633c513e9dc8dac6246ef0ffb
vivinkurien <43483959+vivinkurien@users.noreply.github.com>
2018-09-21 16:33:59 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,37 +1,18 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var BOT_TOKEN = process.env.BOT_TOKEN; 
-var prefix='!';
+
 
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-  // docs refer to as the "ClientUser".
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
+  console.log('Bot has started'); 
+  });
 
 
 client.on("message", async message => {
-    if(message.author.bot) return;
+   
+ console.log(message.content);
   
-    if(message.content.indexOf(prefix) !== 0) return;
-  
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  
-  console.log('blaj');
-  console.log(command);
-  // Let's go with a few common example commands! Feel free to delete or change those.
-  
-  if(command === "ping") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-    const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
-  
- 
 });
 
 client.login(process.env.BOT_TOKEN)
